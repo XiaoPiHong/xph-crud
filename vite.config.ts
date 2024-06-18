42832334d8f87bc0e9e7202d2ab57d9dbb88c131
zhouzhenhong <xiaopihong0325@qq.com>
2024-06-18 09:33:46 +0800

    fix: 打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      entryRoot: path.resolve(__dirname, "packages"),
+      outDir: path.resolve(__dirname, "dist/types"),
+    }),
+  ],
   resolve: {
     alias: [
       { find: "xph-crud", replacement: path.resolve(__dirname, "packages") },
