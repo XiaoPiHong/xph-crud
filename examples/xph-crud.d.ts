3f35359b73052264605dc6e41ce0ffbb349ae80f
zhouzhenhong <xiaopihong0325@qq.com>
2024-06-26 14:18:36 +0800

    fix: 修改

--- examples/xph-crud.d.ts
+++ examples/xph-crud.d.ts
@@ -1,7 +1,7 @@
 import { extend } from "dayjs";
 import { IXphFormProps, IXphActionsProps, TXphTableProps } from "xph-crud";
 
-/** 若需自己扩展组件，可以在这里声明类型(声明后使用该类型定义变量) */
+/** 若需自己扩展组件，可以在这里声明类型(声明后使用该类型定义变量才能获得扩展的组件类型) */
 declare global {
   /** 表单的映射 */
   interface INewXphFormProps extends IXphFormProps<{ MyComponentName: {} }> {}
