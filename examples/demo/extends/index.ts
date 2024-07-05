5083011c1cee989b0b018177026a16b81ac0f655
zhouzhenhong <xiaopihong0325@qq.com>
2024-07-05 16:49:47 +0800

    fix: 修改

--- examples/demo/extends/index.ts
+++ examples/demo/extends/index.ts
@@ -1,11 +1,5 @@
 import ExtendsFormBase from "./extendsFormBase";
 import ExtendsFormItem from "./extendsFormItem";
 import ExtendsTableBase from "./extendsTableBase";
-import ExtendsTableCellFunc from "./extendsTableCellFunc";
 
-export {
-  ExtendsFormBase,
-  ExtendsFormItem,
-  ExtendsTableBase,
-  ExtendsTableCellFunc,
-};
+export { ExtendsFormBase, ExtendsFormItem, ExtendsTableBase };
