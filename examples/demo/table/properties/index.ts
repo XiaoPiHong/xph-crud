a66f5e1165d80a9a49dbfe85e58764d0d506a6ef
zhouzhenhong <xiaopihong0325@qq.com>
2024-10-25 11:51:52 +0800

    fix: 新增案例

--- examples/demo/table/properties/index.ts
+++ examples/demo/table/properties/index.ts
@@ -1,9 +1,11 @@
 import table from "./table";
 import Ref from "./ref";
 import SearchForm from "./searchForm";
+import CrudFormDialog from "./crudFormDialog";
 
 export default {
   table,
   Ref,
   SearchForm,
+  CrudFormDialog,
 };
